ae34afb28b73337fb5701e016ec198d6d37a70a1
tuddman <scott.tudd@gmail.com>
2017-08-09 13:02:59 -0400

    adds vimrc and profiles.clj

---
+++ lein/profiles.clj
@@ -0,0 +1,15 @@
+{:user  
+  {:dependencies [[lein-cljfmt "0.5.6"]]
+   :plugins  [[lein-ancient "0.6.10"]
+              [lein-codox "0.10.3"]
+              [lein-create-template "0.2.0"]
+              [lein-localrepo "0.5.3"]
+              [lein-try "0.4.3"]
+              [lein-kibit "0.1.2"]
+              [jonase/eastwood "0.2.3"]
+              [lein-release-4digit-version "0.1.0"]
+              [lein-instant-cheatsheet "2.2.1"]
+              ]
+          ;:repl-options  {:init  (require 'cljfmt.core)}
+          }}
+
